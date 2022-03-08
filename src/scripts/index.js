e3726aeb1016640330a1e2d6eb4066bdc4291938
Adrian Zuanic <azuanic@gmail.com>
2022-03-09 01:06:45 +1100

    continued experimenting with CSS

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,22 @@
-
-
-const burgerMenuButton = document.querySelector(".burger-menu-button");
+const burgerMenuButton = document.querySelector(".burger-menu-button")
 const mobileMenu = document.querySelector(".mobile-nav-menu")
 
-let menuOpen = false;
-burgerMenuButton.addEventListener('click', () => {
-    console.log("click");
-    menuOpen = !menuOpen;
-    menuOpen === true ? mobileMenu.style.display = "block" : mobileMenu.style.display = "none";
+let menuOpen = false
+burgerMenuButton.addEventListener("click", () => {
+  console.log("click")
+  menuOpen = !menuOpen
+  menuOpen === true
+    ? (mobileMenu.style.display = "block")
+    : (mobileMenu.style.display = "none")
+})
+
+const animatedGradient = document.querySelector(".gradient-anim")
+const contactButton = document.querySelector(".contact-btns")
+contactButton.childNodes.forEach((button) => {
+  button.addEventListener("mouseenter", () => {
+    animatedGradient.style.opacity = ".2"   
+  })
+  button.addEventListener("mouseleave", () => {
+    animatedGradient.style.opacity = "1"
+  })
 })
